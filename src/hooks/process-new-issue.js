30d5ca81ec40ae7a05026c24a4c39d3e18f52ff0
Marco Coetzer <marco.coetzer@entelect.co.za>
2018-03-15 10:54:30 +0200

    DB Fixes, Issue Post Validation Ammened

--- src/hooks/process-new-issue.js
+++ src/hooks/process-new-issue.js
@@ -8,12 +8,60 @@ module.exports = function (options = {}) {
     const { data } = context;
 
 
+    var Ajv = require('ajv');
+    var ajv = new Ajv({ allErrors: true });
 
-    return context;
+    var schema = {
+      "type": "object",
+      "properties": {
+        "title": { "type": "string" },
+        "series": { "type": "string" },
+        "description": { "type": "string" },
+        "publisherString": { "type": "string" },
+        "publicationDate": { "type": "string", "format": "date-time" },
 
-  
+        "thumbnail": {
+          "type": "object",
+          "properties": {
+            "path": { "type": "string" },
+            "extension": { "type": "string" },
+          },
+          "required": ["path", "extension"],
+          "additionalProperties": false
+        },
 
+        "images": {
+          "type": "array",
+          "maxItems": 150,
+          "items": {
+            "type": "object",
+            "properties": {
+              "path": { "type": "string" },
+              "extension": { "type": "string" }
+            },
+            "required": ["path", "extension"],
+            "additionalProperties": false
+          }
+        }
+      },
+      "required": [ "title", "series", "description", "publisherString", "publicationDate" ],
+      "additionalProperties": false
+    };
 
+    var validate = ajv.compile(schema);
 
+    test(data);
+
+    function test(testData) {
+      var valid = validate(testData);
+      if (valid) {
+        console.log('Valid!');
+        return context;
+      }
+      else {
+        console.log('Invalid: ' + ajv.errorsText(validate.errors));
+        throw new Error('Issue POST failed: ' + ajv.errorsText(validate.errors));
+      }
+    }
   };
 };
