61af05f066ed91a28c8bb9faef909c234356f30a
Marco Coetzer <marco.coetzer@entelect.co.za>
2018-03-15 12:55:09 +0200

    Minor Validation  Changes

--- src/hooks/process-issue-edit.js
+++ src/hooks/process-issue-edit.js
@@ -4,6 +4,63 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = function (options = {}) {
   return async context => {
-    return context;
+
+    const { data } = context;
+
+
+    var Ajv = require('ajv');
+    var ajv = new Ajv({ allErrors: true });
+
+    var schema = {
+      "type": "object",
+      "properties": {
+        "title": { "type": "string" },
+        "series": { "type": "string" },
+        "description": { "type": "string" },
+        "publisherString": { "type": "string" },
+        "publicationDate": { "type": "string", "format": "date-time" },
+
+        "thumbnail": {
+          "type": "object",
+          "properties": {
+            "path": { "type": "string" },
+            "extension": { "type": "string" },
+          },
+          "required": ["path", "extension"],
+          "additionalProperties": false
+        },
+
+        "images": {
+          "type": "array",
+          "maxItems": 150,
+          "items": {
+            "type": "object",
+            "properties": {
+              "path": { "type": "string" },
+              "extension": { "type": "string" }
+            },
+            "required": ["path", "extension"],
+            "additionalProperties": false
+          }
+        }
+      },
+      "additionalProperties": false
+    };
+
+    var validate = ajv.compile(schema);
+
+    test(data);
+
+    function test(testData) {
+      var valid = validate(testData);
+      if (valid) {
+        console.log('Valid!');
+        return context;
+      }
+      else {
+        console.log('Invalid: ' + ajv.errorsText(validate.errors));
+        throw new Error('Issue Update failed: ' + ajv.errorsText(validate.errors));
+      }
+    }
   };
 };
--- src/hooks/process-stock-create.js
+++ src/hooks/process-stock-create.js
@@ -4,6 +4,42 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = function (options = {}) {
   return async context => {
-    return context;
+
+    const { data } = context;
+
+
+    var Ajv = require('ajv');
+    var ajv = new Ajv({ allErrors: true });
+
+    var schema = {
+      "type": "object",
+      "properties": {
+
+        "issueId": { "type": "string" , "pattern": "^[A-Za-z0-9_-]{16}*$" },
+        "condition": { "enum": [ "Poor" , "Average" , "Fine" , "Very Fine" ] },
+        "stockAvailable": { "type": "integer" },
+        "price": { "type": "number" },
+
+      },
+
+      "required": [ "issueId", "condition", "stockAvailable", "price" ],
+      "additionalProperties": false
+    };
+
+    var validate = ajv.compile(schema);
+
+    test(data);
+
+    function test(testData) {
+      var valid = validate(testData);
+      if (valid) {
+        console.log('Valid!');
+        return context;
+      }
+      else {
+        console.log('Invalid: ' + ajv.errorsText(validate.errors));
+        throw new Error('Stock create failed: ' + ajv.errorsText(validate.errors));
+      }
+    }
   };
 };
