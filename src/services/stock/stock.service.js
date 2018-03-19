21db9f7da669be4b4c8c1981bd542e329971dc1b
Marco Coetzer <marco.coetzer@entelect.co.za>
2018-03-19 10:26:29 +0200

    Modified and Changed Swagger Stuff

--- src/services/stock/stock.service.js
+++ src/services/stock/stock.service.js
@@ -13,8 +13,153 @@ module.exports = function (app) {
     paginate
   };
 
+  const events = createService(options)
+  events.docs = {
+    description: 'A Service to manage the Stock stored in the database',
+
+    find: {
+      parameters: [
+        {
+          description: 'Number of results to return',
+          in: 'query',
+          name: '$limit',
+          type: 'integer'
+        },
+        {
+          description: 'Number of results to skip',
+          in: 'query',
+          name: '$skip',
+          type: 'integer'
+        },
+        {
+          description: 'Property to sort results',
+          in: 'query',
+          name: '$sort',
+          type: 'string'
+        },
+        {
+          description: 'Property to query results',
+          in: 'query',
+          name: '$search',
+          type: 'string'
+        }
+      ]
+    },
+
+    get: {
+      parameters:
+        [
+          {
+            name: "_id",
+            description: "Stock ID",
+            in: "path",
+            required: true,
+            type: 'string'          
+          }
+        ]
+    },
+
+    remove: {
+      parameters:
+        [
+          {
+            name: "_id",
+            description: "Stock ID",
+            in: "path",
+            required: true,
+            type: 'string'          
+          }
+        ]
+    },
+
+    create: {
+      parameters:
+        [
+          {
+            name: "Post Stock",
+            description: "Stock Object",
+            in: "body",
+            required: true,
+            schema: { $ref: '#/definitions/stockPOST' }
+          }
+        ]
+    },
+
+    update: {
+      parameters:
+        [
+          {
+            name: "_id",
+            description: "Stock ID",
+            in: "path",
+            required: true,
+            type: 'string'          
+          },
+          {
+            name: "Stock Update",
+            description: "Stock Object",
+            in: "body",
+            required: true,
+            schema: { $ref: '#/definitions/stockUPDATE' }
+          }
+        ]
+    },
+
+    patch: {
+      parameters:
+        [
+          {
+            name: "_id",
+            description: "Stock ID",
+            in: "path",
+            required: true,
+            type: 'string' , "pattern": "^[a-zA-Z0-9]{16}$"          
+          },
+          {
+            name: "Stock Patch",
+            description: "Stock Object",
+            in: "body",
+            required: true,
+            schema: { $ref: '#/definitions/stockUPDATE' }
+          }
+        ]
+    },
+
+    definitions: {
+      stockPOST: {
+        "type": "object",
+        "properties": {
+  
+          "issueId": { "type": "string" , "pattern": "^[a-zA-Z0-9]{16}$" },
+          "condition": { "type": "string" , "enum": [ "Poor" , "Average" , "Fine" , "Very Fine" ] },
+          "stockAvailable": { "type": "integer" },
+          "price": { "type": "number" },
+  
+        },
+  
+        "required": [ "issueId", "condition", "stockAvailable", "price" ],
+        "additionalProperties": false
+      },
+
+      stockUPDATE :{
+        "type": "object",
+        "properties": {
+  
+          "issueId": { "type": "string", "pattern": "^[a-zA-Z0-9]{16}$" },
+          "condition": { "type": "string" , "enum": ["Poor", "Average", "Fine", "Very Fine"] },
+          "stockAvailable": { "type": "integer" },
+          "price": { "type": "number" },
+  
+        },
+  
+        "additionalProperties": false
+      }
+      
+    }
+  }
+
   // Initialize our service with any options it requires
-  app.use('/stock', createService(options));
+  app.use('/stock', events);
 
   // Get our initialized service so that we can register hooks and filters
   const service = app.service('stock');
